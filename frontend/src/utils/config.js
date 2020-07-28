780db5c84de30893ac8ccf7b9475db251a9035ae
Jofry Sutanto <jofrysutanto@gmail.com>
2020-07-28 21:56:08 +1000

    Fix incorrectly set ajax url

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,6 +1,8 @@
 let ajaxurl = window.ajaxurl
 
-if (import.meta.env.VITE_WP_AJAX) {
+if (window.ajaxurl) {
+  ajaxurl = window.ajaxurl
+} else if (import.meta.env.MODE === 'development' && import.meta.env.VITE_WP_AJAX) {
   ajaxurl = import.meta.env.VITE_WP_AJAX
 }
 
